4540b6faa2d835f48128060b66a257aa5f03857f
jayprajapati009 <jayprajapati9900@gmail.com>
2022-10-11 16:15:45 -0400

    Output after changes

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,4 +1,5 @@
-#include <AnalogSensor.hpp>
+#include "AnalogSensor.hpp"
+#include <cstdlib>
 #include <numeric>
 #include <vector>
 
@@ -16,6 +17,11 @@ int AnalogSensor::Read()
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+
+    // Cleanup
+    // cleanup:
+    if (readings) {delete readings;}
+
     return result;
 }
 
